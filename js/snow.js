434ef33e4a5b18268d719619219e9dc0b4cb3511
= <gorge.raccoon@gmail.com>
2017-05-23 21:31:33 +0200

    Reformat files

--- js/snow.js
+++ js/snow.js
@@ -1,22 +1,33 @@
-var scene, camera, renderer;
+/*global THREE*/
+var scene, camera, renderer, controls;
 
 init();
 animate();
 
 function init() {
-    scene = new THREE.Scene();
-    
-    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 100 );
-    camera.position.set(0, 5, 10);
-    
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    
-    document.body.appendChild(renderer.domElement);
-}   
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(
+    60,
+    window.innerWidth / window.innerHeight,
+    1,
+    100
+  );
+  camera.position.set(0, 5, 10);
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  controls = new THREE.OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.5;
+  controls.enableZoop = true;
+
+  document.body.appendChild(renderer.domElement);
+}
 
 function animate() {
-    requestAnimationFrame(animate);
-    
-    renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
 }
+
