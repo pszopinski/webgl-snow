4c0bf686c86ae28ae969fb96a7042837d9c2094b
= <gorge.raccoon@gmail.com>
2017-06-05 20:07:23 +0200

    Add params and GUI

--- js/snow.js
+++ js/snow.js
@@ -1,50 +1,80 @@
-/*global THREE*/
-var scene, camera, renderer, stats, controls;
+var scene, camera, renderer, stats, gui, params, controls;
 
 init();
 animate();
 
 function init() {
-  scene = new THREE.Scene();
+    // Scene
+    scene = new THREE.Scene();
 
-  camera = new THREE.PerspectiveCamera(
-    60,
-    window.innerWidth / window.innerHeight,
-    1,
-    100
-  );
-  camera.position.set(0, 5, 10);
+    // Camera
+    camera = new THREE.PerspectiveCamera(
+        60,
+        window.innerWidth / window.innerHeight,
+        1,
+        100
+    );
+    camera.position.set(0, 5, 10);
 
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+    // Renderer
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-  stats = new Stats();
-  document.body.appendChild(stats.dom);
+    // Stats
+    stats = new Stats();
+    document.body.appendChild(stats.dom);
 
-  controls = new THREE.OrbitControls(camera, renderer.domElement);
-  controls.enableDamping = true;
-  controls.dampingFactor = 0.5;
-  controls.enableZoom = true;
+    // Params
+    params = {
+        'Number of snowflakes': 1000,
+        'Downward motion': 5,
+        'Brownian motion': 2
+    };
 
-  window.addEventListener('resize', onWindowResize, false);
+    // GUI
+    gui = new dat.GUI({ width: 300 });
+    var snowflakesFolder = gui.addFolder('Snowflakes');
+    snowflakesFolder.open();
+    var sceneFolder = gui.addFolder('Scene');
+    sceneFolder.open();
+    
+    snowflakesFolder
+        .add(params, 'Number of snowflakes', 500, 5000)
+        .step(100)
+        .onChange(val => console.log('Number of snowflakes: ' + val));
+    snowflakesFolder
+        .add(params, 'Downward motion', 0, 10)
+        .onChange(val => console.log('Downward motion: ' + val));
+    snowflakesFolder
+        .add(params, 'Brownian motion', 0, 10)
+        .onChange(val => console.log('Brownian motion: ' + val));
+
+    // Controls
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.5;
+    controls.enableZoom = true;
+
+    // Resize
+    window.addEventListener('resize', onWindowResize, false);
 }
 
 function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function animate() {
-  requestAnimationFrame(animate);
+    requestAnimationFrame(animate);
 
-  controls.update();
+    controls.update();
 
-  stats.update();
+    stats.update();
 
-  renderer.render(scene, camera);
+    renderer.render(scene, camera);
 }
 
